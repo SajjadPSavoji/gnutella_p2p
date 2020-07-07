7b2e181fa86eebc88c55bfc19db644eb73e44498
sj.pakdaman <sj.pakdaman@ut.ac.ir>
2020-07-07 16:19:12 +0430

    bigi2

--- Neighbor.py
+++ Neighbor.py
@@ -1,12 +1,13 @@
 from utils import *
 class Neighbor():
-    def __init__(self, id, ip, port, last_sent_to_time, last_recv_from_time):
+    def __init__(self, address, last_sent_to_time, last_recv_from_time):
+        '''
+        address = (ip, port)
+        '''
         self.__dict__ = {}
-        self.__dict__[id] = id
-        self.__dict__[ip] = ip
-        self.__dict__[port] = port
-        self.__dict__[last_sent_to_time] = last_sent_to_time
-        self.__dict__[last_recv_from_time] = last_recv_from_time
+        self.__dict__['address'] = address
+        self.__dict__["last_sent_to_time"] = last_sent_to_time
+        self.__dict__['last_recv_from_time'] = last_recv_from_time
 
     def __repr__(self):
         return json.dumps(self.__dict__).encode()
