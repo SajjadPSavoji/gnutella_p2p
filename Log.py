ade98e5b0f9660aa2bffbbf33fbe19f05b3006f7
sj.pakdaman <sj.pakdaman@ut.ac.ir>
2020-07-19 12:25:09 +0430

    Log fixed

--- Log.py
+++ Log.py
@@ -5,7 +5,8 @@ class Log(dict):
     procedures: [DUMP, RCV, SEND, UPDATE, SEARCH, PcktLoss]
     '''
     def __init__(self, procedure, address, hosts_neighbors, time):
-        super().__init__(time=time, procedure=procedure, address=address, neighbors=hosts_neighbors)
+        super().__init__(time=time, procedure=procedure, address=address,
+         neighbors=copy.deepcopy(hosts_neighbors))
 
 if __name__ == "__main__":
     pass
